f46c5e9e5b20607bce5332964bed38a8e1623dc3
Clovis Reschke <clovisreschke@gmail.com>
2021-08-17 15:10:31 -0300

    Corrigido as keys

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region     = "${var.aws_region}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  shared_credentials_file = "/$HOME/.aws/credentials"
+  profile                 = "default"
 }
+
--- variables.tf
+++ variables.tf
@@ -2,15 +2,6 @@ variable "project_name"{
     default = "scheduler-instance"
 }
 
-# AWS Config
-variable "aws_access_key" {
-  default = "AKIAVXU7XO5YPCANUEP6"
-}
-
-variable "aws_secret_key" {
-  default = "HxS56SsqG1LNVySa0WM7CftTv/acufGo1LYP/wGl"
-}
-
 variable "aws_region" {
   default = "us-west-2"
 }
